d9658e8e71642a72a9bdc0d1c2b1d24987d90b5a
Elaina Huang <64375965+yh0010@users.noreply.github.com>
2022-04-28 23:40:11 -0400

    Delete db/feedback directory

--- db/feedback/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-center mt-5">
-  Updated: 2022/04/27
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
